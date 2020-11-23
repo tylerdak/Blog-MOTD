76c0b85824f516394fc72afa8ef46cd6c3ea7f81
tylerdak <dakint@icloud.com>
2020-11-23 11:17:12 -0500

    Initial Commit

---
+++ Blog-MOTD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blog-MOTD
+//
+//  Created by Tyler Dakin on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
